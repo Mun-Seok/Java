4ee51444bd8d7f1ec4c2dfe8c5962da2a4834917
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-28 19:56:47 +0900

    day 02

---
+++ day01/day02/Day02input.java
@@ -0,0 +1,20 @@
+package day02;
+//1.준비물: 입력기능을 내 코드에 연결시켜줘
+import java.util.Scanner;
+public class Day02input {
+
+	public static void main(String[] args) {
+		//2.준비물: scanner를 sc라는 이름으로 사용하겠다
+		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.next());//문자열로 입력받아라 -> 기능 자체가 키보드로 입력한 입력값
+//		System.out.println(sc.nextInt());
+//정수값으로 입력받아라
+//		System.out.println(sc.nextFloat());
+		//실수값으로 입력받아라
+		//System.out.println(sc.next());
+		System.out.println(sc.nextLine());
+		//띄어쓰기까지 포함해서 입력받아라
+
+	}
+
+}
