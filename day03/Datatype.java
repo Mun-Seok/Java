95ffd690d30bbe708aab906a6e531ca30f664393
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-29 19:00:03 +0900

    day03

---
+++ day03/Datatype.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		int a = 65;
+		float f= a;//묵시적형변환(자동형변환)
+		System.out.println(f);
+		char c=(char)a;
+		System.out.println(c);//명시적형변환(강제형변환)
+		// TODO Auto-generated method stub
+
+	}
+
+}
