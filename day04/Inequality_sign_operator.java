ec2806da8bddbd1598b0b406a763b494cc493f47
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-31 16:06:33 +0900

    day04

---
+++ day04/Inequality_sign_operator.java
@@ -0,0 +1,18 @@
+package day04;
+
+public class Inequality_sign_operator {
+
+	public static void main(String[] args) {
+//		*비교연산자 p.44
+		int a=10; //a변수에 10 저장
+		int b=20;//b변수에 20저장
+		int c=30;//c변수에 30저장
+		
+		System.out.println(a<b);//a가 b보다 작아?
+		System.out.println(a>b);//a가 b보다 커?
+		System.out.println(a+b<=c);//a+b가 c보다 작거나 같아?
+		System.out.println(a+b>=c);//a+b가 c보다  크거나 같아?
+
+	}
+
+}
