c2b52028f0a9be88b830e59d258ff72d59433a35
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-31 16:07:01 +0900

    day04

---
+++ day04/Logic_operator.java
@@ -0,0 +1,21 @@
+package day04;
+
+public class Logic_operator {
+
+	public static void main(String[] args) {
+//		*논리 연산자 p.42
+		
+		boolean a=true; //a 변수에 true 값 저장
+		boolean b=false;//b변수에 false 값 저장
+		boolean c=!b; //c변수에 b가 true라면 false, false라면 true
+		
+		System.out.println(a);
+		System.out.println(!a);
+		
+		System.out.println(b);
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
