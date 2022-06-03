d30a5c60cd4fd0f583b6239ef4ede9e0c5310948
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-03 22:28:01 +0900

    day06

---
+++ day06/Three_operator.java
@@ -0,0 +1,13 @@
+package day06;
+
+public class Three_operator {
+
+	public static void main(String[] args) {
+//		*삼항 연산자 p.49
+		int age =20; //정수형 변수 age에 17값 저장
+		System.out.println(age>19?"성인입니다":"청소년입니다");
+		
+
+	}
+
+}
