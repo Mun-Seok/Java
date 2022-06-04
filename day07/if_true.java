0b4e957a8fd1df7b34ffff23c7466328bc1f4c22
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-04 10:06:48 +0900

    day07

---
+++ day07/if_true.java
@@ -0,0 +1,12 @@
+package day07;
+
+public class if_true {
+
+	public static void main(String[] args) {
+		if(true) {//참이라는 값이 들어가 항상 출력이됨 꼭 식만 들어가는것이 아님
+			System.out.println("참입니다");
+		
+		}
+	}
+
+}
