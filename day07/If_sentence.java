eaf3d438bb4a9188cbc10c83d7e0e7e45fa08f41
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-04 10:06:31 +0900

    day07

---
+++ day07/If_sentence.java
@@ -0,0 +1,14 @@
+package day07;
+
+public class If_sentence {
+
+	public static void main(String[] args) {
+		int a=5;//a라는 변수에 5 대입 a에 2를 대입하면 if문 실행x
+		if(a>3) { //a의 값이 3보다 크냐?
+			System.out.println("a는 3보다 큽니다");
+		}//참이므로 해당 명령이 실행됨
+		System.out.println("검사가 끝났습니다");//if문과 상관없이 실행
+
+	}
+
+}
