e1cd3caf376fedaf35f9c82858667654149e22fc
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-06 22:09:55 +0900

    day09

---
+++ day09/For2.java
@@ -0,0 +1,21 @@
+package day09;
+
+public class For2 {
+
+	public static void main(String[] args) {
+//		int sum=0;
+//		for(int i=1;i<=10;i++) {
+//			System.out.printf("i=%d, sum=%d\n",i,sum+=i);
+//		}
+//		1~10까지의 합을 계산해서 출력
+		int sum=0;
+		sum=1;
+		sum=sum+1;
+		sum=sum+2;
+		sum=sum+3;
+		sum=sum+4;
+		}
+
+	}
+
+
