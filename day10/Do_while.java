2a8f06b8ca05f1260dd2cc237cc9f92ff0d6e475
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-07 20:06:45 +0900

    day10

---
+++ day10/Do_while.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Do_While {
+
+	public static void main(String[] args) {
+//		do-while ->false여도 한번은 출력
+//		int i=100;
+//		do {
+//			System.out.println("적어도 한번은 출력되는 문장");
+//		}while(i<10);
+//		System.out.println("종료");
+//		
+//		while ->false면 한번도 출력 안됨
+		int i=100;
+		while(i<10) {
+			System.out.println("적어도 한번은 출력되는 문장");
+		}
+		System.out.println("종료");
+	}
+
+}
