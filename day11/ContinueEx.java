1f128da040389971b9ef81cbb029e6c0d06ddb1d
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-09 10:17:56 +0900

    day11

---
+++ day11/ContinueEx.java
@@ -0,0 +1,15 @@
+package day11;
+
+import java.util.Scanner;
+
+public class ContinueEx {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println();
+		}
+	}
+
+}
