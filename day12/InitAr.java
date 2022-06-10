38808af975587ce7b1789be3bfc76cdc254f90b1
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-10 17:53:28 +0900

    day12

---
+++ day12/InitAr.java
@@ -0,0 +1,26 @@
+package day12;
+
+public class InitAr {
+
+	public static void main(String[] args) {
+		int arInt[]=new int[3];
+		double arDouble[]=new double[3];
+		char arChar[]=new char[3];
+		String arString[]=new String[3];
+		
+		for(int i=0;i<3;i++) {
+			System.out.println(arInt[i]+" ");
+		}
+		for(int i=0;i<3;i++) {
+			System.out.println(arDouble[i]+" ");
+		}
+		for(int i=0;i<3;i++) {
+			System.out.println(arChar[i]+" ");
+		}
+		for(int i=0;i<3;i++) {
+			System.out.println(arString[i]+" ");
+		}
+		
+	}
+
+}
