b0f82a922f18e681d6d194351cdadfb30491d3cd
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-06-15 16:26:49 +0900

    day17

---
+++ day17/Method2.java
@@ -0,0 +1,12 @@
+package day17;
+
+public class Method2 {
+
+	public static void main(String[] args) {
+		System.out.println(f());
+	}
+	static int f() {//자료형도 정수
+		System.out.println("f() 호출!");
+		return 100;//결과값 정수
+	}
+}
