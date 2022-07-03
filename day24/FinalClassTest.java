6f483ed453297ccbf98c76e61cd720f167749b36
ChoiMunSeok <105057510+Mun-Seok@users.noreply.github.com>
2022-07-03 21:58:26 +0900

    day24

---
+++ day24/FinalClassTest.java
@@ -0,0 +1,12 @@
+package day24;
+
+public class FinalClassTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+final class Parent{}
+//class Child extends Parent{}
+//final class 상속 받을수 없음
