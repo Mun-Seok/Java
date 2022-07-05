d6a77014ea6f410a1269b781b5ed17e272159306
ChoiMunSeok <105057510+Mun-Seok@users.noreply.github.com>
2022-07-05 22:43:22 +0900

    day25

---
+++ day25/Downcasting1.java
@@ -0,0 +1,13 @@
+package day25;
+
+public class Downcasting1 {
+
+	public static void main(String[] args) {
+		//업캐스팅
+		Human h1=new Student("가길동",20,"게임하기",3);
+		//다운캐스팅
+		Student s1=(Student)h1;
+		s1.study();//다운 캐스팅해 자식클래스의 것 호출할 수 잇음
+	}
+
+}
