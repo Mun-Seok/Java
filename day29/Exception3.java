ca96ca46a058f94bc7f743973807af02f82c2f25
ChoiMunSeok <105057510+Mun-Seok@users.noreply.github.com>
2022-07-11 15:44:58 +0900

    day29

---
+++ day29/Exception3.java
@@ -0,0 +1,18 @@
+package day29;
+
+public class Exception3 {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=2;
+		try {
+			System.out.println("외부로 접속");
+			int c=b/3;
+		}catch(ArithmeticException e) {
+			System.out.println("오류가 발생하였습니다.");
+		}finally {
+			System.out.println("무조건 연결해제");
+		}
+	}
+
+}
