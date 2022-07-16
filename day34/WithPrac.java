244ab79d5b9c742e6e8eabdb7fc378083fa9f961
ChoiMunSeok <105057510+Mun-Seok@users.noreply.github.com>
2022-07-16 13:09:46 +0900

    day34

---
+++ day34/WithPrac.java
@@ -0,0 +1,12 @@
+package day34;
+
+import java.time.LocalDate;
+
+public class WithPrac {
+	public static void main(String[] args) {
+		LocalDate ld=LocalDate.now();
+		System.out.println(ld);
+		LocalDate new_ld=ld.withYear(1999).withMonth(1).withDayOfYear(23);
+		System.out.println(new_ld);
+	}
+}
