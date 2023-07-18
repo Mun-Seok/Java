0eb8b58e357e218f65235e6646b5d41c60ebab69
Mun-Seok <anstjr97@gmail.com>
2023-07-18 17:15:28 +0900

    변수 예제 2 (두 변수의 교환)

--- Variable/src/VarEx2.java
+++ Variable/src/VarEx2.java
@@ -1,5 +1,6 @@
 public class VarEx2 {
     public static void main(String[] args) {
+
         int x = 10, y = 20; // 변수 x와 y에 각각 10 20으로 초기화
         int temp = 20; // 변수 temp를 20으로 초기화
 
@@ -10,5 +11,6 @@ public class VarEx2 {
         y = temp; // 변수 temp에 저장된 값을 변수 y에 저장
 
         System.out.println("x : " + x + " y : " + y); // 변수 x와 y의 값 출력   x : 20 y : 10
+
     }
 }
