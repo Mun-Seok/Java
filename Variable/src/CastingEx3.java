c8f4bfc894288caad37bf21fce6b7aca43c3689f
Mun-Seok <anstjr97@gmail.com>
2023-07-23 19:53:58 +0900

    VariableEX12

--- Variable/src/CastingEx3.java
+++ Variable/src/CastingEx3.java
@@ -2,10 +2,10 @@ public class CastingEx3 {
     public static void main(String[] args) {
         float f = 9.1234567f;
         double d = 9.1234567;
-        double d2 = (double) f;
+        double d2 = (double) f; // float 타입을 double 타입으로 형변환
 
-        System.out.printf("f = %20.18\n",f);
-        System.out.printf("d = %20.18\n",d);
-        System.out.printf("d2 = %20.18\n",d2);
+        System.out.printf("f = %20.18f\n",f); // f = 9.123456954956055000
+        System.out.printf("d = %20.18f\n",d); // d = 9.123456700000000000
+        System.out.printf("d2 = %20.18f\n",d2); // d2 = 9.123456954956055000
     }
 }
