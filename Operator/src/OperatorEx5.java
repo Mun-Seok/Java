d5e85b9a9f8203f154ebc6aaa9e990483f7bf24b
Mun-Seok <anstjr97@gmail.com>
2023-07-26 19:57:11 +0900

    OperatorEx5(Arthimetic1)

---
+++ Operator/src/OperatorEx5.java
@@ -0,0 +1,12 @@
+public class OperatorEx5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+
+        System.out.printf("%d + %d = %d%n", a, b, a + b); // 10 + 4 =14
+        System.out.printf("%d - %d = %d%n", a, b, a - b); // 10 - 4 = 6
+        System.out.printf("%d * %d = %d%n", a, b, a * b); // 10 * 4 = 40
+        System.out.printf("%d / %d = %d%n", a, b, a / b); // 10 / 4 = 2
+        System.out.printf("%d / %f = %f%n", a, (float) b, a / b); // 10 / 4.000000 = 2.500000
+    }
+}
