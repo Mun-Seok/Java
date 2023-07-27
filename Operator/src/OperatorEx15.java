9d88cd10312460441965aa1bea1d79570645cbe7
Mun-Seok <anstjr97@gmail.com>
2023-07-27 20:06:36 +0900

    OperatorEx15(Arthimetic)

---
+++ Operator/src/OperatorEx15.java
@@ -0,0 +1,7 @@
+public class OperatorEx15 {
+    public static void main(String[] args) {
+        char lowerCase = 'a';
+        char upperCase = (char) (lowerCase - 32); // 소문자를 대문자로 변환
+        System.out.println(upperCase); // A
+    }
+}
