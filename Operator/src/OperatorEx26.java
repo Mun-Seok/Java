ed5bc12f19ce6109ab3f027c1592eaef600dceea
Mun-Seok <anstjr97@gmail.com>
2023-07-28 23:46:01 +0900

    OperatorEx26(Logical)

---
+++ Operator/src/OperatorEx26.java
@@ -0,0 +1,12 @@
+public class OperatorEx26 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        System.out.printf("a = %d, b = %d %n", a, b); // a = 5, b = 0
+        System.out.printf("a != || ++b != 0 = %b%n", a != 0 || ++b != 0); // a != 0 || ++b !=0 = true
+        System.out.printf("a = %d, b = %d %n", a, b); // a = 5, b = 0
+        System.out.printf("a == 0 && ++b != 0 = %b%n", a == 0 && ++b != 0); // a == 0 || ++b !=0 = false
+        System.out.printf("a = %d, b = %d %n", a, b); // a = 5, b = 0
+    }
+}
