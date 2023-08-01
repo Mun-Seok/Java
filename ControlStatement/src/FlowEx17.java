e7bf408c6c58b41f714e2086408efe62485c781a
Mun-Seok <anstjr97@gmail.com>
2023-08-01 16:41:26 +0900

    FlowEx17(for-for)

---
+++ ControlStatement/src/FlowEx17.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FlowEx17 {
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.print("*을 출력할 라인의 수를 입력하세요.>");
+
+        Scanner scanner = new Scanner(System.in);
+        String temp = scanner.nextLine(); // 입력받은 내용을 temp에 저장
+        num = Integer.parseInt(temp);
+
+        for(int i=0;i<num;i++){
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
