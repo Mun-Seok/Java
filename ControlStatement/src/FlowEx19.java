0476e5b3059ab0247bb8dbc5309f2f8ae3f1768a
Mun-Seok <anstjr97@gmail.com>
2023-08-01 16:51:25 +0900

    FlowEx20(for-for)

--- ControlStatement/src/FlowEx19.java
+++ ControlStatement/src/FlowEx19.java
@@ -1,6 +1,6 @@
 public class FlowEx19 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++)
+        for (int i = 1; i <= 3; i++) // 블럭 {} 생략
             for (int j = 1; j <= 3; j++)
                 for (int k = 1; k <= 3; k++)
                     System.out.println(""+i+j+k); // 문자열을 더해 문자열로 바꿔줌
