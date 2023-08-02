5f8cc07107b8a829c6c045d8eb078e4eee6d5374
Mun-Seok <anstjr97@gmail.com>
2023-08-02 22:04:31 +0900

    ArrayEx2

---
+++ Array/src/ArrayEx2.java
@@ -0,0 +1,11 @@
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        int[] iArr1 = new int[10];
+        int[] iArr2 = new int[10];
+        int[] iArr3 = new int[10];
+//        int[] iArr3 = new int[10];
+
+        char[] chArr = {'a', 'b', 'c', 'd'};
+
+    }
+}
