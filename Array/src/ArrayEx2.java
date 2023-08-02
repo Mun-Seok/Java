002f04ec4856bd9174f2633f3875c875ded82183
Mun-Seok <anstjr97@gmail.com>
2023-08-02 22:36:06 +0900

    ArrayEx2

--- Array/src/ArrayEx2.java
+++ Array/src/ArrayEx2.java
@@ -1,11 +1,32 @@
+import java.util.Arrays;
+
 public class ArrayEx2 {
     public static void main(String[] args) {
+        // 배열에 값을 저장하고 출력하는 예제
         int[] iArr1 = new int[10];
         int[] iArr2 = new int[10];
-        int[] iArr3 = new int[10];
-//        int[] iArr3 = new int[10];
+//        int[] iArr3 = new int[]{100, 95, 80, 70, 60};
+        int[] iArr3 = {100, 95, 80, 70, 60}; // new int[] 생략
 
         char[] chArr = {'a', 'b', 'c', 'd'};
 
+        for (int i = 0; i < iArr1.length; i++) {
+            iArr1[i] = i + 1; // 1~10의 숫자를 순서대로 넣는다.
+        }
+
+        for (int i = 0; i < iArr2.length; i++) {
+            iArr2[i] = (int) (Math.random() * 10) + 1; // 1~10의 값을 배열에 저장
+        }
+
+        for (int i = 0; i < iArr1.length; i++) {
+            System.out.print(iArr1[i] + ",");
+        }
+
+        System.out.println();
+        System.out.println(Arrays.toString(iArr2));
+        System.out.println(Arrays.toString(iArr3));
+        System.out.println(Arrays.toString(chArr));
+        System.out.println(iArr3);
+        System.out.println(chArr);
     }
 }
