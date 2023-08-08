ff249dba4d0682ed7be099fc15d4ca430bd0b9e4
Mun-Seok <anstjr97@gmail.com>
2023-08-08 21:52:52 +0900

    PackageEx1

---
+++ OOP2/src/PackageTest.java
@@ -0,0 +1,7 @@
+// 패키지 선언하는 예
+//package com.java.practice; // -d 옵션 활용해 컴파일 해주멷 된다.
+public class PackageTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
