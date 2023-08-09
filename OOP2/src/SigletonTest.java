ce6557539610afc719591a1089c04c3a905aa1e7
Mun-Seok <anstjr97@gmail.com>
2023-08-09 15:54:24 +0900

    ModifierEX3(Access)

---
+++ OOP2/src/SigletonTest.java
@@ -0,0 +1,18 @@
+// 생정자의 접근 제어자를 private으로 지정하고 메서드로 인스턴스를 생성하는 예제
+final class Singleton{
+    private static Singleton s = new Singleton();
+    private Singleton(){
+        // ...
+    }
+    public static Singleton getInstance(){
+        if (s == null) {
+            s = new Singleton();
+        }
+        return s;
+    }
+}
+public class SigletonTest {
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+    }
+}
