6fd1fb8ab9804b886c68703d03e8fb737c8a1e77
Mun-Seok <anstjr97@gmail.com>
2023-08-17 15:36:44 +0900

    InterfaceEx6

---
+++ OOP2/src/InterfaceTest3.java
@@ -0,0 +1,31 @@
+// InstanceManger의 getInstance() 메서드를 사용하는 예제
+public class InterfaceTest3 {
+    public static void main(String[] args) {
+        A3 a = new A3();
+        a.methodA();
+    }
+}
+class A3{
+    void methodA(){
+        I3 i = InstanceManager.getInstance();
+        i.methodB();
+        System.out.println(i.toString()); // i로 Object 클래스의 메서드 호출가능
+    }
+}
+interface I3 {
+    public abstract void methodB();
+}
+
+class B3 implements I3 {
+    public void methodB(){
+        System.out.println("methodB in B class");
+    }
+    public String toString() {
+        return "class B";
+    }
+}
+class InstanceManager{
+    public static I3 getInstance() {
+        return new B3();
+    }
+}
