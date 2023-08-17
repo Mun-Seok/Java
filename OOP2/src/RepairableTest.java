8da45617a3fed71442a2c8e3236e866171d082a7
Mun-Seok <anstjr97@gmail.com>
2023-08-17 15:10:36 +0900

    InterfaceEx3

--- OOP2/src/RepairableTest.java
+++ OOP2/src/RepairableTest.java
@@ -1,33 +1,36 @@
+// 인터페이스를 사용해 클래스들에게 관계를 맺어주는 예제
 public class RepairableTest {
     public static void main(String[] args) {
         Tank tank = new Tank();
         Dropship dropship = new Dropship();
 
         Marine marine = new Marine();
-        Scv scv = new Scv();
+        SCV scv = new SCV();
 
         scv.repair(tank);
         scv.repair(dropship);
-        scv.repair(marine);
+//        scv.repair(marine);
     }
 }
 interface Repairable {}
-class Unit {
+class Unit2 {
     int hitPoint;
     final int MAX_HP;
-    Unit(int hp){
+    Unit2(int hp){
         MAX_HP = hp;
     }
     // ...
 }
-class GroundUnit extends Unit{
+class GroundUnit extends Unit2{
     GroundUnit(int hp) {
         super(hp);
     }
 }
 
-class AirUnit(int hp) {
-    super(hp);
+class AirUnit extends Unit2 {
+    AirUnit(int hp) {
+        super(hp);
+    }
 }
 
 class Tank extends GroundUnit implements Repairable {
@@ -37,5 +40,43 @@ class Tank extends GroundUnit implements Repairable {
     }
     public String toString() {
         return "Tank";
+    }
+    // ...
+}
+class Dropship extends AirUnit implements Repairable {
+    Dropship(){
+        super(125);
+        hitPoint = MAX_HP;
+    }
+
+    public String toString() {
+        return "Dropship";
+    }
+    // ...
+}
+
+class Marine extends GroundUnit {
+    Marine() {
+        super(40);
+        hitPoint = MAX_HP;
+    }
+    // ...
+}
+
+class SCV extends GroundUnit implements Repairable {
+    SCV() {
+        super(60);
+        hitPoint = MAX_HP;
+    }
+
+    void repair(Repairable repairable) {
+        if (repairable instanceof Unit2) {
+            Unit2 unit = (Unit2) repairable;
+            while (unit.hitPoint != unit.MAX_HP) {
+                /* Unit의 HP를 증가시킨다. */
+                unit.hitPoint++;
+            }
+            System.out.println(unit.toString() + "의 수리가 끝났습니다.");
+        }
     }
 }
