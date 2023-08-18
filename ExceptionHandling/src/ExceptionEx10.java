2bcb82b428136bbfce264af3f6db14c3da8a8e30
Mun-Seok <anstjr97@gmail.com>
2023-08-18 21:37:47 +0900

    ExceptionHandlingEx10

---
+++ ExceptionHandling/src/ExceptionEx10.java
@@ -0,0 +1,6 @@
+// 예외를 발생시키고 예외 처리하지 않았을 때 결과를 보는 예제
+/*public class ExceptionEx10 {
+    public static void main(String[] args) {
+        throw new Exception();
+    } // 예외처리가 되어 있지 않다는 에러가 발생
+}*/
