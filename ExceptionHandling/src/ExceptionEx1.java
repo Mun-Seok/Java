e787e760afa9bb34da152f82e3515fdaf3f9b19f
Mun-Seok <anstjr97@gmail.com>
2023-08-18 11:20:09 +0900

    ExceptionHandlingEx1

---
+++ ExceptionHandling/src/ExceptionEx1.java
@@ -0,0 +1,19 @@
+// try-catch문 사용하는 법을 보여주는 예제
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try {
+            } catch (Exception e) {
+
+            }
+        } catch (Exception e) {
+
+        }
+
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
+}
