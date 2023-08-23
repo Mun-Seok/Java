62b0b2bd5a183712fa513c141cd445704cbe1d80
Mun-Seok <anstjr97@gmail.com>
2023-08-23 12:58:40 +0900

    ExceptionHandlingEx18

---
+++ ExceptionHandling/src/FinallyTest2.java
@@ -0,0 +1,20 @@
+// finally 블럭을 사용해 임시파일들을 삭제해주는 예제
+public class FinallyTest2 {
+    public static void main(String[] args) {
+        try {
+            startInstall();
+            copyFiles();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            deleteTempFiles();
+        }
+    }
+    static void startInstall() {
+        /* 프로그램 설치에 필요한 준비를 하는 코드를 적는다. */
+    }
+    static void copyFiles() {
+        /* 파일들을 복사하는 코드를 적는다. */
+    }
+    static void deleteTempFiles() { /* 임시파일들을 삭제하는 코드를 적는다. */ }
+}
