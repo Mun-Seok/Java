ca9c7161dc00dae9be6d02f98cdaea547851229e
Mun-Seok <anstjr97@gmail.com>
2023-08-28 14:53:55 +0900

    ExceptionHandlingEx22

---
+++ ExceptionHandling/src/ExceptionEx17.java
@@ -0,0 +1,18 @@
+// 예외 되던지기를 통해 예외처리해주는 예제
+public class ExceptionEx17 {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("main메서드에서 예외가 처리되었습니다.");
+        }
+    }
+    static void method1() throws Exception {
+        try{
+            throw new Exception();
+        } catch(Exception e){
+            System.out.println("method1메서드에서 예외가 처리되었습니다.");
+            throw e;
+        }
+    }
+}
