4ba95e2faddf9feb3cc9f20b84314bdd4eb9d283
Mun-Seok <anstjr97@gmail.com>
2023-09-01 21:45:49 +0900

    UtilClassEx12

---
+++ UtilClasses/src/StringEx2.java
@@ -0,0 +1,9 @@
+// 문자열 리터럴을 보여주는 예제
+public class StringEx2 {
+    public static void main(String[] args) {
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+    }
+}
