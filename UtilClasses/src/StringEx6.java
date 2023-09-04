67e6ae7780cf7e96f57134b69a5d2cbb65dbb8ed
Mun-Seok <anstjr97@gmail.com>
2023-09-04 13:35:04 +0900

    UtilClassEx16

---
+++ UtilClasses/src/StringEx6.java
@@ -0,0 +1,17 @@
+// 문자열과 기본형 간의 변환을 보여주는 예제
+public class StringEx6 {
+    public static void main(String[] args) {
+        int iVal = 100;
+        String strVal = String.valueOf(iVal); // int를 String으로 변환한다.
+
+        double dVal = 200.0;
+        String strVal2 = dVal + ""; // String으로 변환하는 또 다른 방법
+
+        double sum = Integer.parseInt("+" + strVal) + Double.parseDouble(strVal2);
+
+        double sum2 = Integer.valueOf(strVal) + Double.valueOf(strVal2);
+
+        System.out.println(String.join("", strVal, "+", strVal2, "=") + sum);
+        System.out.println(strVal + "+" + strVal2 + "=" + sum2);
+    }
+}
