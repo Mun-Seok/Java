ee23148e73ca56e4de903f07408db405b32bf373
Mun-Seok <anstjr97@gmail.com>
2023-09-05 13:24:50 +0900

    UtilClassEx18

---
+++ UtilClasses/src/StringBufferEx1.java
@@ -0,0 +1,16 @@
+// StringBuffer 인스턴스에 equals()와 toString() 메서드를 사용해보고 비교하는 예제
+public class StringBufferEx1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("abc");
+        StringBuffer sb2 = new StringBuffer("abc");
+
+        System.out.println("sb == sb2 ? " + (sb == sb2));
+        System.out.println("sb.equals(sb2) ? " + sb.equals(sb2));
+
+        // StringBuffer의 내용을 String으로 변환한다.
+        String s = sb.toString(); // String s = new String(sb);와 같다.
+        String s2 = sb2.toString();
+
+        System.out.println("s.equals(s2) ? " + s.equals(s2));
+    }
+}
