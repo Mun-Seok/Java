c3f64d032d6de92423c1b6528f7d20f15c625e69
Mun-Seok <anstjr97@gmail.com>
2023-09-08 13:53:25 +0900

    FormattingEx6

--- Formatting/src/DateFormatEx3.java
+++ Formatting/src/DateFormatEx3.java
@@ -6,7 +6,7 @@ public class DateFormatEx3 {
         DateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일");
         DateFormat df2 = new SimpleDateFormat("yyyy/MM/dd");
         try {
-            Date d = df.parse("2023년 9월 17일");
+            Date d = df.parse("2023년 9월 17일"); // 문자열을 Date 인스턴스로 변환
             System.out.println(df2.format(d));
         } catch (Exception e) { }
     }
