aa1e1a306a266b3f23cc664122401144e20fa450
Mun-Seok <anstjr97@gmail.com>
2023-09-08 13:04:33 +0900

    FormattingEx2

---
+++ Formatting/src/DecimalFormatEx2.java
@@ -0,0 +1,18 @@
+import java.text.*;
+d .
+public class DecimalFormatEx2 {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#,###.##");
+        DecimalFormat df2 = new DecimalFormat("#.###E0");
+
+        try {
+            Number num = df.parse("1,234,567.89");
+            System.out.printf("1,234,567.89");
+
+            double d = num.doubleValue();
+            System.out.print(d + " → ");
+
+            System.out.println(df2.format(num));
+        } catch (Exception e) {}
+    }
+}
