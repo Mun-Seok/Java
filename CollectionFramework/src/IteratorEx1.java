6ba3f78ee0e0e8fb326c39d9591cb4aaa3ffec86
Mun-Seok <anstjr97@gmail.com>
2023-09-19 21:16:08 +0900

    CollectionFrameworkEx13

---
+++ CollectionFramework/src/IteratorEx1.java
@@ -0,0 +1,19 @@
+// List 클래스를 Iterator를 이용해 읽어 온 결과를 출력하는 예제
+import java.util.*;
+public class IteratorEx1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+        Iterator it = list.iterator();
+
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.println(obj);
+        }
+    }
+}
