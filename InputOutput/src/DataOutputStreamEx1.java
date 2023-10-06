8207d7810bc803964bacbdf0a9a859626f5b3ce4
Mun-Seok <anstjr97@gmail.com>
2023-10-06 20:02:32 +0900

    InputOutputEx8

---
+++ InputOutput/src/DataOutputStreamEx1.java
@@ -0,0 +1,20 @@
+// DataOutputStream을 생성하고 메서드를 이용해 sample.dat 파일에 값을 출력하는 예제
+import java.io.*;
+public class DataOutputStreamEx1 {
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        DataOutputStream dos = null;
+
+        try {
+            fos = new FileOutputStream("sample.dat");
+            dos = new DataOutputStream(fos);
+            dos.writeInt(10);
+            dos.writeFloat(20.0f);
+            dos.writeBoolean(true);
+
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
