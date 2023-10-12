c89668635c2b70ce3fdfa4df8f846b89e4b22e9b
Mun-Seok <anstjr97@gmail.com>
2023-10-12 10:09:52 +0900

    InputOutputEx14

---
+++ InputOutput/src/SequenceInputStreamEx.java
@@ -0,0 +1,35 @@
+// ByteArrayInpuStream을 Vector와 SequenceInputStream을 이용해 하나의 입력스트림처럼 다루는 예제
+import java.util.*;
+import java.io.*;
+public class SequenceInputStreamEx {
+    public static void main(String[] args) {
+        byte[] arr1 = {0, 1, 2};
+        byte[] arr2 = {3, 4, 5};
+        byte[] arr3 = {6, 7, 8};
+        byte[] outSrc = null;
+
+        Vector v = new Vector();
+        v.add(new ByteArrayInputStream(arr1));
+        v.add(new ByteArrayInputStream(arr2));
+        v.add(new ByteArrayInputStream(arr3));
+
+        SequenceInputStream input = new SequenceInputStream(v.elements());
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        int data = 0;
+
+        try {
+            while ((data = input.read()) != -1) {
+                output.write(data); // void write(int b
+            }
+        } catch (IOException e) { }
+
+        outSrc = output.toByteArray();
+
+        System.out.println("Input Source1 : " + Arrays.toString(arr1));
+        System.out.println("Input Source2 : " + Arrays.toString(arr2));
+        System.out.println("Input Source3 : " + Arrays.toString(arr3));
+        System.out.println("Output Source : " + Arrays.toString(outSrc));
+
+    }
+}
