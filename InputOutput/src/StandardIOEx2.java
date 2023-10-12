331a84b2b81759721d988c4acccec29997cfd062
Mun-Seok <anstjr97@gmail.com>
2023-10-12 12:33:49 +0900

    InputOutputEx22

---
+++ InputOutput/src/StandardIOEx2.java
@@ -0,0 +1,7 @@
+// System.out과 System.err을 이용해 출력해보는 예제
+public class StandardIOEx2 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");
+    }
+}
