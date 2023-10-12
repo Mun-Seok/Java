241f5bdc91c360590ee5d6a099769cd1e2a29c73
Mun-Seok <anstjr97@gmail.com>
2023-10-12 10:45:46 +0900

    InputOutputEx15

---
+++ InputOutput/src/PrintStreamEx1.java
@@ -0,0 +1,23 @@
+// PrintStream의 메서드를 사용해 값들을 출력해보는 예제
+import java.util.Date;
+public class PrintStreamEx1 {
+    public static void main(String[] args) {
+        int i = 65;
+        float f = 1234.56789f;
+
+        Date d = new Date();
+
+        System.out.printf("문자 %c의 코드는 %d%n", i, i);
+        System.out.printf("%d는 8진수로 %o, 16진수는 %x%n", i, i, i);
+        System.out.printf("%3d%3d%3d\n", 100, 90, 80);
+        System.out.println();
+        System.out.printf("123456789012345678901234567890%n");
+        System.out.printf("%s%-5s%5s%n", "123", "123", "123");
+        System.out.println();
+        System.out.printf("%-8.1f%8.1f %e%n", f, f, f);
+        System.out.println();
+        System.out.printf("오늘은 %tY년 %tm월 %td일 입니다. %n", d, d, d);
+        System.out.printf("지금은 %tH시 %tM분 %tS초 입니다. %n", d, d, d);
+        System.out.printf("지금은 %1$tH시 %1$tM분 %1$tS초 입니다. %n", d); // 1$ 첫번째 매개변수를 뜻함
+    }
+}
