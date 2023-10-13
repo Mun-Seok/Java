3f20048fd89af9c8d1a5ac7b845452b7b5e3596b
Mun-Seok <anstjr97@gmail.com>
2023-10-13 10:28:23 +0900

    InputOutputEx34

---
+++ InputOutput/src/FileEx8.java
@@ -0,0 +1,42 @@
+// 재구호출을 이용해 특정 확장자 가진 파일 delete로 삭제하는 예제
+import java.io.*;
+public class FileEx8 {
+    static int deletedFiles = 0;
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("USAGE : java FileEX8 Extension");
+            System.exit(0);
+        }
+
+        String currDir = System.getProperty("user.dir");
+
+        File dir = new File(currDir);
+        String ext = "." + args[0];
+
+        delete(dir, ext);
+        System.out.println(deletedFiles + "개의 파일이 삭제되었습니다.");
+    }
+
+    public static void delete(File dir, String ext) {
+        File[] files = dir.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                    delete(files[i], ext);
+                } else {
+                String filename = files[i].getAbsolutePath();
+
+                if (filename.endsWith(ext)) {
+                    System.out.print(filename);
+                    if (files[i].delete()) {
+                        System.out.println(" - 삭제 성공");
+                        deletedFiles++;
+                    } else {
+                        System.out.println(" - 삭제 실패");
+                    }
+                }
+            }
+        }
+    }
+}
